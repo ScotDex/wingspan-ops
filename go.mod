055c3900a3a44dfee260ac85b9067dfc6f0e0287
ScotDex <themadlyscientific@gmail.com>
2025-10-13 17:11:27 +0100

    authentication

--- go.mod
+++ go.mod
@@ -2,4 +2,10 @@ module wingspan-ops
 
 go 1.25.2
 
-require github.com/joho/godotenv v1.5.1
+require (
+	github.com/gorilla/sessions v1.4.0
+	github.com/joho/godotenv v1.5.1
+	golang.org/x/oauth2 v0.32.0
+)
+
+require github.com/gorilla/securecookie v1.1.2 // indirect
--- internal/server/server.go
+++ internal/server/server.go
@@ -6,8 +6,12 @@ import (
 	"path/filepath"
 	"wingspan-ops/internal/esi"
 	"wingspan-ops/internal/routing"
+
+	"github.com/gorilla/sessions"
+	"golang.org/x/oauth2"
 )
 
+// functions provides custom functions to be used within the HTML templates.
 var functions = template.FuncMap{
 	"HTML": func(s string) template.HTML {
 		return template.HTML(s)
@@ -17,46 +21,75 @@ var functions = template.FuncMap{
 	},
 }
 
-// The Server struct now holds a map of templates.
+// Server holds all the dependencies required for the web application.
 type Server struct {
-	templates   map[string]*template.Template
-	wingspanURL string
-	feedbackURL string
-	esiClient   *esi.ESIClient
-	graph       *routing.Graph
+	templates    map[string]*template.Template
+	wingspanURL  string
+	feedbackURL  string
+	esiClient    *esi.ESIClient
+	graph        *routing.Graph
+	oauthConfig  *oauth2.Config
+	sessionStore *sessions.CookieStore
 }
 
-func New(wingspanAPIURL, feedbackURL string, esiClient *esi.ESIClient, graph *routing.Graph) (*Server, error) {
-	// The new cache function returns a map.
+// New creates and initializes a new Server instance.
+func New(
+	wingspanAPIURL, feedbackURL string,
+	esiClient *esi.ESIClient,
+	graph *routing.Graph,
+	oauthConfig *oauth2.Config,
+	sessionStore *sessions.CookieStore,
+) (*Server, error) {
+	// Initialize the template cache.
 	cache, err := newTemplateCache("./templates")
 	if err != nil {
 		return nil, err
 	}
 
+	// Create and return the Server instance with all dependencies.
 	return &Server{
-		templates:   cache,
-		wingspanURL: wingspanAPIURL,
-		feedbackURL: feedbackURL,
-		esiClient:   esiClient,
-		graph:       graph,
+		templates:    cache,
+		wingspanURL:  wingspanAPIURL,
+		feedbackURL:  feedbackURL,
+		esiClient:    esiClient,
+		graph:        graph,
+		oauthConfig:  oauthConfig,
+		sessionStore: sessionStore,
 	}, nil
 }
 
+// RegisterRoutes sets up all the HTTP routes for the application.
 func (s *Server) RegisterRoutes() http.Handler {
 	mux := http.NewServeMux()
-	mux.HandleFunc("/", s.homeHandler)
-	mux.HandleFunc("/short-circuit", s.shortCircuitHandler)
-	mux.HandleFunc("/lookup", s.lookupHandler)
-	mux.HandleFunc("/about", s.aboutHandler) // ADD THIS
+
+	// --- Public Routes ---
+	// These routes are accessible to everyone, logged in or not.
+
+	// 1. Static file server for CSS, images, etc.
 	fileServer := http.FileServer(http.Dir("./static"))
 	mux.Handle("/static/", http.StripPrefix("/static/", fileServer))
+
+	// 2. Authentication handlers for the SSO login and callback flow.
+	mux.HandleFunc("/login", s.loginHandler)
+	mux.HandleFunc("/callback", s.callbackHandler)
+	mux.HandleFunc("/logout", s.logoutHandler)
+
+	// --- Protected Routes ---
+	// These routes are wrapped in the authMiddleware. If a user is not
+	// logged in, the middleware will redirect them to the login page.
+	mux.Handle("/", s.authMiddleware(http.HandlerFunc(s.homeHandler)))
+	mux.Handle("/short-circuit", s.authMiddleware(http.HandlerFunc(s.shortCircuitHandler)))
+	mux.Handle("/lookup", s.authMiddleware(http.HandlerFunc(s.lookupHandler)))
+	mux.Handle("/about", s.authMiddleware(http.HandlerFunc(s.aboutHandler)))
+
 	return mux
 }
 
+// newTemplateCache parses all templates and stores them in a map for efficient rendering.
 func newTemplateCache(dir string) (map[string]*template.Template, error) {
 	cache := make(map[string]*template.Template)
 
-	// Find all "page" templates (anything that isn't a layout/partial).
+	// Find all "page" templates (e.g., index.html, about.html).
 	pages, err := filepath.Glob(filepath.Join(dir, "*.html"))
 	if err != nil {
 		return nil, err
@@ -64,18 +97,18 @@ func newTemplateCache(dir string) (map[string]*template.Template, error) {
 
 	for _, page := range pages {
 		name := filepath.Base(page)
-		// Skip files that are not "pages" (like layouts).
+		// Skip files that are not meant to be rendered as standalone pages.
 		if name == "layout.html" {
 			continue
 		}
 
-		// Create a new template set for each page.
-		// Start with the base layout, then add the specific page file.
+		// For each page, create a new template set that includes the main layout.
 		ts, err := template.New(name).Funcs(functions).ParseFiles(filepath.Join(dir, "layout.html"), page)
 		if err != nil {
 			return nil, err
 		}
 		cache[name] = ts
 	}
+
 	return cache, nil
 }
